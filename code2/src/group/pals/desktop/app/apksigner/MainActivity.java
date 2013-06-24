cca40867b40d2e0e744cc00e4dfe3aff6b0ee04d
hai bison <hai bison@localhost>
2013-06-25 04:37:22 +0700

    Ported UI for PanelKeygen.

--- code2/src/group/pals/desktop/app/apksigner/MainActivity.java
+++ code2/src/group/pals/desktop/app/apksigner/MainActivity.java
@@ -10,6 +10,8 @@ package group.pals.desktop.app.apksigner;
 import group.pals.desktop.app.apksigner.i18n.Messages;
 import group.pals.desktop.app.apksigner.ui.Dlg;
 import group.pals.desktop.app.apksigner.ui.JEditorPopupMenu;
+import group.pals.desktop.app.apksigner.utils.Files;
+import group.pals.desktop.app.apksigner.utils.Preferences;
 import group.pals.desktop.app.apksigner.utils.UI;
 
 import java.awt.EventQueue;
@@ -17,6 +19,7 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.InputEvent;
 import java.awt.event.KeyEvent;
+import java.io.File;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
@@ -30,6 +33,7 @@ import javax.swing.SpringLayout;
 import javax.swing.SwingUtilities;
 import javax.swing.UIManager;
 import javax.swing.UnsupportedLookAndFeelException;
+import javax.swing.border.TitledBorder;
 
 import de.muntjak.tinylookandfeel.Theme;
 import de.muntjak.tinylookandfeel.ThemeDescription;
@@ -43,8 +47,20 @@ import de.muntjak.tinylookandfeel.TinyLookAndFeel;
  */
 public class MainActivity {
 
+    /**
+     * The class name.
+     */
+    private static final String CLASSNAME = MainActivity.class.getName();
+
+    /**
+     * This key holds the last working directory.
+     */
+    private static final String PKEY_LAST_WORKING_DIR = CLASSNAME
+            + ".last_working_dir";
+
     private JFrame mMainFrame;
     private JTextField mTextJdkPath;
+    private JTabbedPane mTabbedPane;
 
     /**
      * Launch the application.
@@ -72,6 +88,7 @@ public class MainActivity {
         /*
          * THEME
          */
+
         try {
             ThemeDescription[] availableThemes = Theme.getAvailableThemes();
             for (ThemeDescription td : availableThemes) {
@@ -89,6 +106,20 @@ public class MainActivity {
 
         UI.setWindowCenterScreen(mMainFrame, 59);
         UI.setEditorPopupMenu(mMainFrame, new JEditorPopupMenu());
+
+        /*
+         * INITIALIZE CONTROLS
+         */
+
+        mMainFrame.setTitle(String.format(Messages.getString("pmsg_app_name"),
+                Messages.getString("app_name"),
+                Messages.getString("app_version_name")));
+
+        File jdkPath = Preferences.getInstance().getJdkPath();
+        if (jdkPath != null && jdkPath.isDirectory())
+            mTextJdkPath.setText(jdkPath.getAbsolutePath());
+
+        mTabbedPane.add(Messages.getString("key_generator"), new PanelKeygen());
     }// MainActivity()
 
     /**
@@ -126,7 +157,7 @@ public class MainActivity {
             @Override
             public void actionPerformed(ActionEvent e) {
                 String msg = String
-                        .format("%s %s\n\n"
+                        .format("%s\n\n"
                                 + "...by Hai Bison\n\n"
                                 + " - License: MIT License\n"
                                 + " - Code page: https://code.google.com/p/apk-signer/\n"
@@ -138,8 +169,10 @@ public class MainActivity {
                                 + "\n"
                                 + "And thanks to our friends who have been contributing to this project:\n"
                                 + " - Leo Chien (https://plus.google.com/118055781130476825691?prsrc=2)",
-                                Messages.getString("app_name"),
-                                Messages.getString("app_version_name"));
+                                String.format(
+                                        Messages.getString("pmsg_app_name"),
+                                        Messages.getString("app_name"),
+                                        Messages.getString("app_version_name")));
                 Dlg.showHugeInfoMsg(null, null, msg, 630, 270);
             }// actionPerformed()
         });
@@ -148,6 +181,10 @@ public class MainActivity {
         mMainFrame.getContentPane().setLayout(springLayout);
 
         mTextJdkPath = new JTextField();
+        mTextJdkPath.setEditable(false);
+        mTextJdkPath.setBorder(new TitledBorder(null, Messages
+                .getString("desc_jdk_path"), TitledBorder.LEADING,
+                TitledBorder.TOP, null, null));
         springLayout.putConstraint(SpringLayout.NORTH, mTextJdkPath, 10,
                 SpringLayout.NORTH, mMainFrame.getContentPane());
         springLayout.putConstraint(SpringLayout.WEST, mTextJdkPath, 10,
@@ -156,6 +193,21 @@ public class MainActivity {
 
         JButton mBtnChooseJdkPath = new JButton(
                 Messages.getString("choose_jdk_path")); //$NON-NLS-1$
+        mBtnChooseJdkPath.addActionListener(new ActionListener() {
+
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                File f = Files.chooseDir(new File(Preferences.getInstance()
+                        .get(PKEY_LAST_WORKING_DIR, "/")));
+                Preferences.getInstance().setJdkPath(f);
+                if (f != null) {
+                    Preferences.getInstance().set(PKEY_LAST_WORKING_DIR,
+                            f.getParentFile().getAbsolutePath());
+                    mTextJdkPath.setText(f.getAbsolutePath());
+                } else
+                    mTextJdkPath.setText(null);
+            }// actionPerformed()
+        });
         springLayout.putConstraint(SpringLayout.EAST, mTextJdkPath, -10,
                 SpringLayout.WEST, mBtnChooseJdkPath);
         springLayout.putConstraint(SpringLayout.NORTH, mBtnChooseJdkPath, 10,
@@ -164,17 +216,17 @@ public class MainActivity {
                 SpringLayout.EAST, mMainFrame.getContentPane());
         mMainFrame.getContentPane().add(mBtnChooseJdkPath);
 
-        JTabbedPane mTabbedPane = new JTabbedPane(JTabbedPane.TOP);
+        mTabbedPane = new JTabbedPane(JTabbedPane.TOP);
+        springLayout.putConstraint(SpringLayout.SOUTH, mBtnChooseJdkPath, -10,
+                SpringLayout.NORTH, mTabbedPane);
+        springLayout.putConstraint(SpringLayout.NORTH, mTabbedPane, 10,
+                SpringLayout.SOUTH, mTextJdkPath);
         springLayout.putConstraint(SpringLayout.WEST, mTabbedPane, 5,
                 SpringLayout.WEST, mMainFrame.getContentPane());
         springLayout.putConstraint(SpringLayout.SOUTH, mTabbedPane, -5,
                 SpringLayout.SOUTH, mMainFrame.getContentPane());
         springLayout.putConstraint(SpringLayout.EAST, mTabbedPane, -5,
                 SpringLayout.EAST, mMainFrame.getContentPane());
-        springLayout.putConstraint(SpringLayout.SOUTH, mTextJdkPath, -10,
-                SpringLayout.NORTH, mTabbedPane);
-        springLayout.putConstraint(SpringLayout.NORTH, mTabbedPane, 10,
-                SpringLayout.SOUTH, mBtnChooseJdkPath);
         mMainFrame.getContentPane().add(mTabbedPane);
     }// initialize()
 }
---
+++ code2/src/group/pals/desktop/app/apksigner/utils/Preferences.java
@@ -0,0 +1,146 @@
+/*
+ *    Copyright (C) 2012 Hai Bison
+ *
+ *    See the file LICENSE at the root directory of this project for copying
+ *    permission.
+ */
+
+package group.pals.desktop.app.apksigner.utils;
+
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.util.Properties;
+
+/**
+ * Convenient class for storing/ loading preferences.
+ * 
+ * @author Hai Bison
+ */
+public class Preferences {
+
+    /**
+     * Used for debugging...
+     */
+    private static final String CLASSNAME = Preferences.class.getName();
+
+    private static Preferences mInstance;
+
+    /**
+     * Gets the global instance of this class.
+     * 
+     * @return the global instance of this class.
+     */
+    public static Preferences getInstance() {
+        if (mInstance == null)
+            mInstance = new Preferences();
+        return mInstance;
+    }// getInstance()
+
+    public static final String PREFS_FILENAME = "apk-signer.preferences";
+
+    private final Properties mProperties = new Properties();
+
+    /**
+     * Creates new instance.
+     */
+    private Preferences() {
+        try {
+            FileReader r = new FileReader(Sys.getStartupDir().getAbsolutePath()
+                    + "/" + PREFS_FILENAME);
+            try {
+                mProperties.load(r);
+            } finally {
+                r.close();
+            }
+        } catch (Exception e) {
+            System.err.printf("[%s] Error loading preferences: %s\n",
+                    CLASSNAME, e);
+        }
+    }// Preferences()
+
+    /**
+     * Stores all preferences to file.
+     */
+    public void store() {
+        try {
+            FileWriter w = new FileWriter(Sys.getStartupDir().getAbsolutePath()
+                    + "/" + PREFS_FILENAME);
+            try {
+                mProperties.store(w, null);
+            } finally {
+                w.close();
+            }
+        } catch (Exception e) {
+            System.err.printf("[%s] Error storing preferences: %s\n",
+                    CLASSNAME, e);
+        }
+    }// store()
+
+    /**
+     * Sets a preference.
+     * 
+     * @param k
+     *            the key name.
+     * @param v
+     *            the value of the key.
+     */
+    public void set(String k, String v) {
+        mProperties.setProperty(k, v != null ? v.trim() : "");
+        store();
+    }// set()
+
+    /**
+     * Gets value of a key.
+     * 
+     * @param k
+     *            the key name.
+     * @return the value of the given key.
+     */
+    public String get(String k) {
+        return mProperties.getProperty(k);
+    }// get()
+
+    /**
+     * Gets value of a key.
+     * 
+     * @param k
+     *            the key name.
+     * @param def
+     *            the default value if the given key does not exist.
+     * @return the value of the given key, or {@code def} if the given key does
+     *         not exist.
+     */
+    public String get(String k, String def) {
+        return mProperties.getProperty(k, def);
+    }// get()
+
+    /*
+     * PREFERENCES
+     */
+
+    public static final String KEY_JDK_PATH = "JDK_PATH";
+
+    /**
+     * Gets JDK path.
+     * 
+     * @return the JDK path, or {@code null} if not available.
+     */
+    public File getJdkPath() {
+        String path = get(KEY_JDK_PATH);
+        return path == null ? null : new File(path);
+    }// getJdkPath()
+
+    /**
+     * Sets the JDK path.
+     * 
+     * @param path
+     *            the JDK path.
+     */
+    public void setJdkPath(File path) {
+        if (path != null)
+            set(KEY_JDK_PATH, path.getAbsolutePath());
+        else
+            mProperties.remove(KEY_JDK_PATH);
+    }// setJdkPath()
+}

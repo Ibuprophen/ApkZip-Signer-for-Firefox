339fb152f3a265b8284c2ff6c4a6cfbe4253a604
hai bison <hai bison@localhost>
2013-06-25 08:00:36 +0700

    Finish porting PanelKeyTools.

--- code2/src/group/pals/desktop/app/apksigner/PanelKeyTools.java
+++ code2/src/group/pals/desktop/app/apksigner/PanelKeyTools.java
@@ -8,16 +8,28 @@
 package group.pals.desktop.app.apksigner;
 
 import group.pals.desktop.app.apksigner.i18n.Messages;
+import group.pals.desktop.app.apksigner.ui.Dlg;
+import group.pals.desktop.app.apksigner.ui.JEditorPopupMenu;
+import group.pals.desktop.app.apksigner.utils.Files;
+import group.pals.desktop.app.apksigner.utils.KeyTools;
+import group.pals.desktop.app.apksigner.utils.Preferences;
+import group.pals.desktop.app.apksigner.utils.Texts;
+import group.pals.desktop.app.apksigner.utils.UI;
 
-import javax.swing.JPanel;
-import java.awt.GridBagLayout;
-import javax.swing.JButton;
-import java.awt.GridBagConstraints;
-import javax.swing.JPasswordField;
-import java.awt.Insets;
-import javax.swing.JTextArea;
-import javax.swing.JScrollPane;
 import java.awt.Font;
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
+import java.awt.Insets;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.File;
+
+import javax.swing.JButton;
+import javax.swing.JPanel;
+import javax.swing.JPasswordField;
+import javax.swing.JScrollPane;
+import javax.swing.JTextArea;
+import javax.swing.SwingConstants;
 import javax.swing.border.TitledBorder;
 
 /**
@@ -43,7 +55,21 @@ public class PanelKeyTools extends JPanel {
      */
     private static final String PKEY_LAST_WORKING_DIR = CLASSNAME
             + ".last_working_dir";
-    private JPasswordField passwordField;
+
+    /*
+     * FIELDS
+     */
+
+    private File mKeyfile;
+
+    /*
+     * CONTROLS
+     */
+
+    private JPasswordField mTextPassword;
+    private JButton mBtnChooseKeyfile;
+    private JButton mBtnListEntries;
+    private JTextArea mTextInfo;
 
     /**
      * Create the panel.
@@ -57,31 +83,60 @@ public class PanelKeyTools extends JPanel {
                 Double.MIN_VALUE };
         setLayout(gridBagLayout);
 
-        JButton btnNewButton = new JButton(
+        mBtnChooseKeyfile = new JButton(
                 Messages.getString("desc_load_key_file"));
-        GridBagConstraints gbc_btnNewButton = new GridBagConstraints();
-        gbc_btnNewButton.insets = new Insets(10, 3, 3, 3);
-        gbc_btnNewButton.gridx = 0;
-        gbc_btnNewButton.gridy = 0;
-        add(btnNewButton, gbc_btnNewButton);
+        mBtnChooseKeyfile.addActionListener(new ActionListener() {
 
-        passwordField = new JPasswordField();
-        passwordField.setBorder(new TitledBorder(null, Messages
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                mKeyfile = Files.chooseFile(new File(Preferences.getInstance()
+                        .get(PKEY_LAST_WORKING_DIR, "/")),
+                        Texts.REGEX_KEYSTORE_FILES, Messages
+                                .getString("desc_keystore_files"));
+                if (mKeyfile != null) {
+                    mBtnChooseKeyfile.setText(mKeyfile.getName());
+                    mBtnChooseKeyfile.setForeground(UI.COLOUR_SELECTED_FILE);
+                    Preferences.getInstance().set(PKEY_LAST_WORKING_DIR,
+                            mKeyfile.getParentFile().getAbsolutePath());
+                } else {
+                    mBtnChooseKeyfile.setText(Messages
+                            .getString("desc_load_key_file"));
+                    mBtnChooseKeyfile.setForeground(UI.COLOUR_WAITING_CMD);
+                }
+            }// actionPerformed()
+        });
+        GridBagConstraints gbc_mBtnChooseKeyfile = new GridBagConstraints();
+        gbc_mBtnChooseKeyfile.insets = new Insets(10, 3, 3, 3);
+        gbc_mBtnChooseKeyfile.gridx = 0;
+        gbc_mBtnChooseKeyfile.gridy = 0;
+        add(mBtnChooseKeyfile, gbc_mBtnChooseKeyfile);
+
+        mTextPassword = new JPasswordField();
+        mTextPassword.setHorizontalAlignment(SwingConstants.CENTER);
+        mTextPassword.setBorder(new TitledBorder(null, Messages
                 .getString("password"), TitledBorder.LEADING, TitledBorder.TOP,
                 null, null));
-        GridBagConstraints gbc_passwordField = new GridBagConstraints();
-        gbc_passwordField.insets = new Insets(3, 3, 3, 3);
-        gbc_passwordField.fill = GridBagConstraints.HORIZONTAL;
-        gbc_passwordField.gridx = 0;
-        gbc_passwordField.gridy = 1;
-        add(passwordField, gbc_passwordField);
+        GridBagConstraints gbc_mTextPassword = new GridBagConstraints();
+        gbc_mTextPassword.insets = new Insets(3, 3, 3, 3);
+        gbc_mTextPassword.fill = GridBagConstraints.HORIZONTAL;
+        gbc_mTextPassword.gridx = 0;
+        gbc_mTextPassword.gridy = 1;
+        add(mTextPassword, gbc_mTextPassword);
 
-        JButton btnNewButton_1 = new JButton(Messages.getString("list_entries"));
-        GridBagConstraints gbc_btnNewButton_1 = new GridBagConstraints();
-        gbc_btnNewButton_1.insets = new Insets(3, 3, 3, 3);
-        gbc_btnNewButton_1.gridx = 0;
-        gbc_btnNewButton_1.gridy = 2;
-        add(btnNewButton_1, gbc_btnNewButton_1);
+        mBtnListEntries = new JButton(Messages.getString("list_entries"));
+        mBtnListEntries.addActionListener(new ActionListener() {
+
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                if (validateFields())
+                    listEntries();
+            }// actionPerformed()
+        });
+        GridBagConstraints gbc_mBtnListEntries = new GridBagConstraints();
+        gbc_mBtnListEntries.insets = new Insets(3, 3, 3, 3);
+        gbc_mBtnListEntries.gridx = 0;
+        gbc_mBtnListEntries.gridy = 2;
+        add(mBtnListEntries, gbc_mBtnListEntries);
 
         JScrollPane scrollPane = new JScrollPane();
         GridBagConstraints gbc_scrollPane = new GridBagConstraints();
@@ -91,13 +146,50 @@ public class PanelKeyTools extends JPanel {
         gbc_scrollPane.gridy = 3;
         add(scrollPane, gbc_scrollPane);
 
-        JTextArea textArea = new JTextArea();
-        textArea.setEditable(false);
-        textArea.setFont(new Font("Monospaced", textArea.getFont().getStyle(),
-                textArea.getFont().getSize()));
-        textArea.setMargin(new Insets(9, 9, 9, 9));
-        textArea.setTabSize(4);
-        scrollPane.setViewportView(textArea);
+        mTextInfo = new JTextArea();
+        mTextInfo.setEditable(false);
+        mTextInfo.setFont(new Font("Monospaced",
+                mTextInfo.getFont().getStyle(), mTextInfo.getFont().getSize()));
+        mTextInfo.setMargin(new Insets(9, 9, 9, 9));
+        mTextInfo.setTabSize(4);
+        scrollPane.setViewportView(mTextInfo);
+
+        UI.setEditorPopupMenu(this, new JEditorPopupMenu());
     }// PanelKeyTools()
 
+    /**
+     * Validates all fields.
+     * 
+     * @return {@code true} or {@code false}.
+     */
+    private boolean validateFields() {
+        if (mKeyfile == null || !mKeyfile.isFile() || !mKeyfile.canRead()) {
+            Dlg.showErrMsg(null, null,
+                    Messages.getString("msg_keyfile_doesnt_exist"));
+            mBtnChooseKeyfile.requestFocus();
+            return false;
+        }
+
+        if (mTextPassword.getPassword() == null
+                || mTextPassword.getPassword().length == 0) {
+            Dlg.showErrMsg(null, null,
+                    Messages.getString("msg_password_is_empty"));
+            mTextPassword.requestFocus();
+            return false;
+        }
+
+        return true;
+    }// validateFields()
+
+    /**
+     * Lists all entries of a keyfile.
+     * <p>
+     * <b>Notes:</b> You should call {@link #validateFields()} first.
+     * </p>
+     */
+    private void listEntries() {
+        CharSequence result = KeyTools.listEntries(Preferences.getInstance()
+                .getJdkPath(), mKeyfile, mTextPassword.getPassword());
+        mTextInfo.setText(result.toString().trim());
+    }// listEntries()
 }
